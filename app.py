43ab52bedb307d51605ec21ac023e61a0049006a
Randy Ridgley <rridgley@amazon.com>
2024-01-09 14:14:04 -0500

    fix(docs)

--- app.py
+++ app.py
@@ -1,44 +1,40 @@
 #!/usr/bin/env python3
 import os
-import aws_cdk.pipelines
 
-from aws_cdk import App
-from boto3 import client, session
+import aws_cdk.pipelines
+from aws_cdk import App, Aws
 from cdk_pipelines_github import AwsCredentials, GitHubWorkflow
 
 from cdk.cdk_python_pipeline.pipeline.service_stage import ServiceStage
 from cdk.cdk_python_pipeline.pipeline.setup_stage import SetupStage
 
-account = client('sts').get_caller_identity()['Account']
-region = session.Session().region_name
-environment = os.getenv('ENVIRONMENT', 'dev')
+account = Aws.ACCOUNT_ID
+region = Aws.REGION
+environment = os.getenv("ENVIRONMENT", "dev")
+
 app = App()
 
 pipeline = GitHubWorkflow(
-    app, 'Pipeline',
+    app,
+    "Pipeline",
     synth=aws_cdk.pipelines.ShellStep(
-        'Build',
+        "Build",
         commands=[
-            'yarn install',
-            'make dev',
+            "yarn install",
+            "make dev",
         ],
     ),
     aws_creds=AwsCredentials.from_open_id_connect(
-        git_hub_action_role_arn=f'arn:aws:iam::{account}:role/GitHubActionRole',
+        git_hub_action_role_arn=f"arn:aws:iam::{account}:role/GitHubActionRole",
     ),
 )
 
-setup_stage = SetupStage(
-    scope=app,
-    id='setup-stage')
+setup_stage = SetupStage(scope=app, id="setup-stage")
 
-dev_stage = ServiceStage(
-    scope=app,
-    id='dev-service-stage'
-)
+dev_stage = ServiceStage(scope=app, id="dev-service-stage")
 
 pipeline.add_stage(setup_stage)
-devWave = pipeline.add_wave('Development')
+devWave = pipeline.add_wave("Development")
 devWave.add_stage(dev_stage)
 
 app.synth()
